3cbe38edf4e4d69e29bb837439bd5ce910513712
Md Masum Rana <masummx3@gmail.com>
2022-05-27 22:41:37 +0600

    set data

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const express = require('express');
 const app =express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port =process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ecmxs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        const ProductCollection =client.db('travel_hardware').collection('product')
+        const ReviewCollection =client.db('travel_hardware').collection('Review')
+        app.get('/product', async(req, res)=>{
+            const query ={};
+            const cursor = ProductCollection.find(query);
+            const product= await cursor.toArray()
+            res.send(product);
+        })
+        //product with id
+        app.get('/product/:id', async(req, res)=>{
+            const id =req.params.id;
+            const query ={_id:ObjectId(id)}
+            const product =await ProductCollection.findOne(query);
+            res.send(product);
+        })
+        //review
+        app.get('/review', async(req, res)=>{
+            const query ={};
+            const cursor = ReviewCollection.find(query);
+            const review= await cursor.toArray()
+            res.send(review);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req,res)=>{
     res.send('travel hardware store is ready for product sell')

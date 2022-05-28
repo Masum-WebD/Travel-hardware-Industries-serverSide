80d27f5d3fd0fcf8f81ff50f02c1c32abf7d19dc
Md Masum Rana <masummx3@gmail.com>
2022-05-28 16:35:53 +0600

    try admin and user

--- index.js
+++ index.js
@@ -21,6 +21,23 @@ async function run() {
         const ProductCollection =client.db('travel_hardware').collection('product')
         const ReviewCollection =client.db('travel_hardware').collection('Review')
         const OrdersCollection =client.db('travel_hardware').collection('orders')
+        const userCollection =client.db('travel_hardware').collection('user')
+        
+          app.get("/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === "admin";
+            res.send({ admin: isAdmin });
+          });
+          app.put("/user/admin/:email",  async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+              $set: { role: "admin" },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+          });
         app.get('/product', async(req, res)=>{
             const query ={};
             const cursor = ProductCollection.find(query);
@@ -54,7 +71,22 @@ async function run() {
             const orders = await OrdersCollection.find(query).toArray();
             res.send(orders);
         })
-
+        app.get("/user", async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+          });
+          app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            
+            res.send(result);
+          });
     }
     finally{
 

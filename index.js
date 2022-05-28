25274b6f13b4d0bcf67dd49fa913a33b751fa5d8
Md Masum Rana <masummx3@gmail.com>
2022-05-28 10:01:46 +0600

    make it better

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port =process.env.PORT || 5000;
 
-
+//midleware
 app.use(cors());
 app.use(express.json())
 
